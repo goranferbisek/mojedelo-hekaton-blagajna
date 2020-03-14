c4163c5ab6382e16823ea02eafeed2ed946cca2a
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-03-14 19:57:13 +0100

    Parse input rows

--- Blagajna.java
+++ Blagajna.java
@@ -2,18 +2,31 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.File;
 import java.io.IOException;
+import java.util.regex.*;
 
 class Blagajna {
     public static void main(String[] args) {
         try {
             BufferedReader reader = new BufferedReader(
-            new FileReader(new File("vhodi.txt")));
+            new FileReader(new File("vhodi.txt"))); //vhod
 
             String line;
-            while( (line = reader.readLine()) != null) {
-                System.out.println(line);
+
+            //regex pattern (row length) (rows) (text)
+            Pattern pattern = Pattern.compile("(\\d+)\\s(\\d+)\\s(.+)");
+            Matcher matcher;
+
+            while ( (line = reader.readLine()) != null) {
+                matcher = pattern.matcher(line);
+
+                if (matcher.groupCount() != 3) {
+                    System.out.println("Parsing did not succeed");
+                } else {
+                    System.out.println("Let's get it on");
+                }
             }
 
+            reader.close();
         } catch (IOException e) {
             e.getMessage();
         }
